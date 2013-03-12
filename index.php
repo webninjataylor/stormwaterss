669b5fb9e787ecd840bdb2807163dd7c7b06145d
Taylor Johnson <webninjataylor@gmail.com>
2013-03-12 14:38:29 -0400

    Project started

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>StormWaterss</title>
+    <?php include('includes/meta.php'); ?>
+</head>
+<body>
+    <?php include('includes/header.php'); ?>
+    <?php include('includes/menu.php'); ?>
+    <article>
+        <h1>Under Construction</h1>
+        <p>This website template is mobile-friendly. It's using a responsive design to be flexible enough for handling iPhone portrait and landscape views as well as iPad portrait and landscape views.</p>
+    </article>
+    <?php include('includes/footer.php'); ?>
+</body>
+</html>
