56c1aa1edf8e85144c26c2a11152f0252bd71986
Taylor Johnson <webninjataylor@gmail.com>
2013-03-14 17:23:17 -0400

    Menu links dummied

--- includes/menu.php
+++ includes/menu.php
@@ -1,5 +1,5 @@
 <menu>
     <li><a href="index.php" data-path="/index.php">Home</a></li>
-    <li><a href="about.php" data-path="/about.php">About</a></li>
-    <li><a href="contactus.php" data-path="/contactus.php">Contact Us</a></li>
+    <li><a href="#" data-path="/about.php">About</a></li>
+    <li><a href="#" data-path="/contactus.php">Contact Us</a></li>
 </menu>
