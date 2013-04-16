45491ac0109672b7864eddb4799de3da57ba26a2
Taylor Johnson <webninjataylor@gmail.com>
2013-04-16 13:06:14 -0400

    Fixed Firefox layout.  Menu needed a clear for some reason.

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,5 @@
 <header>
     <a href="index.php"><img src="images/logo.png" id="logo" alt="Logo"></a>
-    <!--<img src="images/header1.jpg" id="grabber" alt="">-->
-    <!--<div id="imageswap"></div>-->
     <div id="cycler">
         <img class="active" src="images/header1.jpg">
         <img src="images/header2.jpg">     
