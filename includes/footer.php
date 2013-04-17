185b0754f3bf8c8bb7fd87ea244f5f69812a0e2b
Taylor Johnson <webninjataylor@gmail.com>
2013-04-17 15:12:16 -0400

    Cachebusting

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <footer>
     Copyright &copy; 2013 Stormwater Solutions &amp; Services, LLC. All rights reserved. &#149;  <a href="https://www.elance.com/s/webninjataylor/">Website by Web Ninja Taylor</a>
 </footer>
-<script type="text/javascript" src="js/main.min.js"></script><!-- ALL other scripts get concatenated and minified automatically -->
+<script type="text/javascript" src="js/main.min.js?cachebust=1"></script><!-- ALL other scripts get concatenated and minified automatically -->
