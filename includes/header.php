121d71295e11c6155f956cbb48c705628258bb68
Taylor Johnson <webninjataylor@gmail.com>
2013-04-17 15:10:53 -0400

    Project text and first image set

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <header>
     <a href="index.php"><img src="images/logo.png" id="logo" alt="Logo"></a>
-    <div id="cycler">
+    <div id="cycler" class="cycler">
         <img class="active" src="images/header1.jpg">
         <img src="images/header2.jpg">     
     </div>
