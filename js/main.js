d8afdcd22ea859d78d6c9a011f9aaff7228bc0e7
Taylor Johnson <webninjataylor@gmail.com>
2013-04-20 19:30:10 -0400

    Project images

--- js/main.js
+++ js/main.js
@@ -28,7 +28,6 @@ function cycleImages(){
 		$next.css('z-index',3).addClass('active');//make the next image the top one
 	});
 }
-
 function cycleImages2(){
 	var $active = $('#cycler2 .active');
 	var $next = ($active.next().length > 0) ? $active.next() : $('#cycler2 img:first');
@@ -38,7 +37,6 @@ function cycleImages2(){
 		$next.css('z-index',3).addClass('active');//make the next image the top one
 	});
 }
-
 function cycleImages3(){
 	var $active = $('#cycler3 .active');
 	var $next = ($active.next().length > 0) ? $active.next() : $('#cycler3 img:first');
@@ -48,10 +46,89 @@ function cycleImages3(){
 		$next.css('z-index',3).addClass('active');//make the next image the top one
 	});
 }
-
+function cycleImages4(){
+	var $active = $('#cycler4 .active');
+	var $next = ($active.next().length > 0) ? $active.next() : $('#cycler4 img:first');
+	$next.css('z-index',2);//move the next image up the pile
+	$active.fadeOut(1500,function(){//fade out the top image
+		$active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+		$next.css('z-index',3).addClass('active');//make the next image the top one
+	});
+}
+function cycleImages5(){
+	var $active = $('#cycler5 .active');
+	var $next = ($active.next().length > 0) ? $active.next() : $('#cycler5 img:first');
+	$next.css('z-index',2);//move the next image up the pile
+	$active.fadeOut(1500,function(){//fade out the top image
+		$active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+		$next.css('z-index',3).addClass('active');//make the next image the top one
+	});
+}
+function cycleImages6(){
+	var $active = $('#cycler6 .active');
+	var $next = ($active.next().length > 0) ? $active.next() : $('#cycler6 img:first');
+	$next.css('z-index',2);//move the next image up the pile
+	$active.fadeOut(1500,function(){//fade out the top image
+		$active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+		$next.css('z-index',3).addClass('active');//make the next image the top one
+	});
+}
+function cycleImages7(){
+	var $active = $('#cycler7 .active');
+	var $next = ($active.next().length > 0) ? $active.next() : $('#cycler7 img:first');
+	$next.css('z-index',2);//move the next image up the pile
+	$active.fadeOut(1500,function(){//fade out the top image
+		$active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+		$next.css('z-index',3).addClass('active');//make the next image the top one
+	});
+}
+function cycleImages8(){
+	var $active = $('#cycler8 .active');
+	var $next = ($active.next().length > 0) ? $active.next() : $('#cycler8 img:first');
+	$next.css('z-index',2);//move the next image up the pile
+	$active.fadeOut(1500,function(){//fade out the top image
+		$active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+		$next.css('z-index',3).addClass('active');//make the next image the top one
+	});
+}
+function cycleImages9(){
+	var $active = $('#cycler9 .active');
+	var $next = ($active.next().length > 0) ? $active.next() : $('#cycler9 img:first');
+	$next.css('z-index',2);//move the next image up the pile
+	$active.fadeOut(1500,function(){//fade out the top image
+		$active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+		$next.css('z-index',3).addClass('active');//make the next image the top one
+	});
+}
+function cycleImages10(){
+	var $active = $('#cycler10 .active');
+	var $next = ($active.next().length > 0) ? $active.next() : $('#cycler10 img:first');
+	$next.css('z-index',2);//move the next image up the pile
+	$active.fadeOut(1500,function(){//fade out the top image
+		$active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+		$next.css('z-index',3).addClass('active');//make the next image the top one
+	});
+}
+function cycleImages11(){
+	var $active = $('#cycler11 .active');
+	var $next = ($active.next().length > 0) ? $active.next() : $('#cycler11 img:first');
+	$next.css('z-index',2);//move the next image up the pile
+	$active.fadeOut(1500,function(){//fade out the top image
+		$active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
+		$next.css('z-index',3).addClass('active');//make the next image the top one
+	});
+}
 $(document).ready(function(){
 	// run every 3s
 	setInterval('cycleImages()', 3000);
 	setInterval('cycleImages2()', 3000);
 	setInterval('cycleImages3()', 3000);
+	setInterval('cycleImages4()', 3000);
+	setInterval('cycleImages5()', 3000);
+	setInterval('cycleImages6()', 3000);
+	setInterval('cycleImages7()', 3000);
+	setInterval('cycleImages8()', 3000);
+	setInterval('cycleImages9()', 3000);
+	setInterval('cycleImages10()', 3000);
+	setInterval('cycleImages11()', 3000);
 });
--- projects.php
+++ projects.php
@@ -20,17 +20,22 @@
         <p>The tight clay soils and significant relief of Stokes County, North Carolina created a unique challenge in treating runoff from a school parking lot. The result was a tiered bioretention facility. The top bioretention cell accepts runoff from the 1.72-acre parking lot, allowing it to infiltration through the 3-foot bioretention media. The media is unique in that washed granite screenings from a nearby quarry were used in lieu of coarse sand, resulting in decreased costs and transportation requirements. Drainage from the top cell, as well as any overflow, is directed to the bottom cell for further treatment. Kathy designed this practice and provided construction oversight.</p>
 
         <h2>Rainwater Harvesting System for Animal Shelter</h2>
-        <div id="cycler3" class="cycler cycler_inline_vertical">
+        <div id="cycler3" class="cycler cycler_inline">
             <img class="active" src="images/as1.jpg">
             <img src="images/as2.jpg">
         </div>
-        <p>A 6,000 gallon rainwater harvesting system was designed and installed at the Craven County Animal Shelter in New Bern, NC. The system accepts runoff from approximately 5,250ft2 and the harvested rainwater is used to flush the trench that drains kennels housing animals awaiting adoption. A municipal backup water supply was included in the design in the event the tanks went dry (which has yet to happen!). During the first 1.5 years of use, the animal shelter utilized 60,000 gallons of rainwater!</p>
+        <p>A 6,000 gallon rainwater harvesting system was designed and installed at the Craven County Animal Shelter in New Bern, NC. The system accepts runoff from approximately 5,250ft<sup>2</sup> and the harvested rainwater is used to flush the trench that drains kennels housing animals awaiting adoption. A municipal backup water supply was included in the design in the event the tanks went dry (which has yet to happen!). During the first 1.5 years of use, the animal shelter used 60,000 gallons of rainwater!</p>
 
         <h2>North Carolina LID Manual Contributions &amp; Spreadsheet Model</h2>
+        <div id="cycler4" class="cycler cycler_inline">
+            <img class="active" src="images/lid1.jpg">
+            <img src="images/lid2.jpg">
+        </div>
         <p>The North Carolina Low Impact Development (LID) Guidebook was published in 2009 by a collection of numerous individuals and agencies. Kathy provided verbiage and guidance for Chapter 2: Achieving LID Performance Goals Using A Hydrologic Cycle Approach. Additionally, Kathy developed a complimentary spreadsheet model that allows users to compare a site's post-development hydrology to three different pre-development scenarios.</p>
         <p><a href="http://www.ces.ncsu.edu/depts/agecon/WECO/lidguidebook/">View more information on the NC LID initiative, Guidebook, and Model.</a></p>
 
         <h2>North Carolina Jordan/Falls Stormwater Nutrient Load Accounting Tool</h2>
+        <img class="content_image_full" src="images/ncjf.jpg">
         <p>Kathy developed a spreadsheet model to be used by developers and municipalities in accordance with new Jordan and Falls Lake regulations in the state of North Carolina. This accounting tool is designed to estimate runoff volume and pollutant loadings from developments and reductions in these loadings provided by various best management practices. While developed specifically for application within the Jordan and Falls lake watersheds, the tool possesses the flexibility to be applied state-wide, but both new development and retrofit applications.</p>
         <p>
             <a href="http://portal.ncdenr.org/web/jordanlake/implementation-guidance-archive">Access the accounting tool and the User's Manual</a><br>
@@ -39,18 +44,41 @@
         </p>
 
         <h2>Rainwater Harvesting System for Irrigation at Country Club</h2>
+        <div id="cycler5" class="cycler cycler_inline">
+            <img class="active" src="images/cc1.jpg">
+            <img src="images/cc2.jpg">
+        </div>
         <p>Kathy designed and installed a 12,000 gallon rainwater harvesting system at River Bend Country Club in New Bern, NC. Runoff was collected from 6,500ft<sup>2</sup> of the pool deck and 6,600ft<sup>2</sup> of roof surface. Due to the flat topography in this coastal area, an underground tank and pump system was used to convey the runoff to the 3 aboveground storage tanks. The collected rainwater was used for irrigating the turfgrass surrounding the pool and tennis courts. Unlike many irrigation applications, this system was designed to irrigate year-round to facilitate stormwater management. This resulted in a substantial reduction in flooding around the tennis courts and maintenance buildings, as well as a reduction in the stormwater fee issued by the town of River Bend.</p>
 
         <h2>Stormwater Wetland for Parking Lot Runoff</h2>
-        <p>This wetland was designed for an industrial processing facility in Wilson County for treatment of stormwater runoff from the adjacent parking lot. The 33,000ft2 wetland is slightly undersized for the contributing drainage area, but still provides substantial mitigation and treatment of the contributing runoff.</p>
+        <div id="cycler6" class="cycler cycler_inline">
+            <img class="active" src="images/wetland1.jpg">
+            <img src="images/wetland2.gif">
+        </div>
+        <p>This wetland was designed for an industrial processing facility in Wilson County for treatment of stormwater runoff from the adjacent parking lot. The 33,000ft<sup>2</sup> wetland is slightly undersized for the contributing drainage area, but still provides substantial mitigation and treatment of the contributing runoff.</p>
 
         <h2>Rainwater Harvesting System for Irrigation &amp; Stormwater Management</h2>
+        <div id="cycler7" class="cycler cycler_inline">
+            <img class="active" src="images/irrigation1.jpg">
+            <img src="images/irrigation2.jpg">
+            <img src="images/irrigation3.jpg">
+        </div>
         <p>This innovative rainwater harvesting system was designed and installed for Tryon Palace in New Bern, NC. Located in a historical district, aesthetics were a critical concern when implementing this system. Accordingly, the system was designed to fit in a hidden alley, between a brick wall and a building, and consisted of 5 Waterwall Fatboy storage tanks connected together via piping. The system was used to irrigate the signature flower gardens at Tryon Palace, with two spigots installed at easily-accessible locations. Overflow from the system was directed to a small rain garden for infiltration.</p>
 
         <h2>Rainwater Harvesting System for Fire Station</h2>
+        <div id="cycler8" class="cycler cycler_inline">
+            <img class="active" src="images/fs1.jpg">
+            <img src="images/fs2.jpg">
+            <img src="images/fs3.gif">
+        </div>
         <p>A retrofit rainwater harvesting system was designed for a fire station in Raleigh, NC for washing fire trucks. The tanks were designed with a wood wrap and metal roof to improve aesthetic appearances.  Kathy provided design and construction oversight services for the project.</p>
 
         <h2>Urban Stormwater Retrofit Feasibility &amp; Economic Study</h2>
+        <div id="cycler9" class="cycler cycler_inline">
+            <img class="active" src="images/urban1.gif">
+            <img src="images/urban2.gif">
+            <img src="images/urban3.gif">
+        </div>
         <p>Kathy was project lead on a multi-municipality feasibility study conducted in North Carolina. The following verbiage was taken from the <a href="http://www.ncsu.edu/wrri/pdfs/research/Projects2008/50382HuntReport.pdf">final project report</a>, which was written by Kathy and Dr. Bill Hunt.</p>
         <block>
             <p>New regulations in the state of North Carolina, USA, require all communities within the Jordan Lake watershed – a water supply watershed – to reduce their nitrogen and phosphorus loadings to the lake by establishing stromwater control measures (SCMs) for new and existing development (NCDENR 2009). It is anticipated that these new regulations will be emulated in many urban areas across the state of North Carolina and the United States. As such, it is important to understand the feasibility, both physically and economically, of implementing such practices. The goal of this project was to evaluate the feasibility and cost-effectiveness of treating stormwater in urban areas that have already been developed. Furthermore, it was necessary to determine if the state-mandated nitrogen and phosphorus reduction goals of 8% and 5%, respectively, for existing development within the watershed were attainable via retrofit stormwater management practices. Seven North Carolina cities, that are members of the Urban Water Consortium Stormwater Group, served as the study locations: Raleigh, Durham, Greensboro, Wilmington, High Point, Winston-Salem and Charlotte. One watershed, between 260 and 520 ha, was selected from each of these cities for analysis. An eighth watershed was chosen from Greensboro as well. A ninth larger watershed was analyzed as well, though emphasis was placed on identifying large, regional SCMs as opposed to smaller, site-specific practices.</p>
@@ -58,9 +86,17 @@
         </block>
 
         <h2>Bioretention Practices for Parking Lot Runoff</h2>
+        <div id="cycler10" class="cycler cycler_inline">
+            <img class="active" src="images/brp1.jpg">
+            <img src="images/brp2.jpg">
+        </div>
         <p>Previously installed stormwater management practices for this were not functioning as originally designed and attended. The practices were analyzed and the following problems identified: (1) little to no ponding depth within the individual bioretention practices, (2) installed media did not adhere to current state specifications, (3) shallow underdrain depths (sometimes less than 2'), allowing little to no internal storage within the cells, (4) substantial undersizing of at least 2 of the existing cells, and (5) substantial erosion in the last cell of the series. Designs plans and cost estimates were developed to restore the existing BMPs to functioning stormwater practices that complied with state regulations.</p>
 
         <h2>Permeable Pavement for Parking Lot Runoff</h2>
+        <div id="cycler11" class="cycler cycler_inline">
+            <img class="active" src="images/pp1.gif">
+            <img src="images/pp2.gif">
+        </div>
         <p>A retrofit permable pavement design was created for a site located in Pender County, NC. The design replaced the stalls of an existing parking lot with permeable interlocking concrete pavers (PICP) that accepted runoff from the driving lane of the lot, resulting in treatment of runoff for the entire lot. The underdrains from the PICP were directed to an existing drop inlet, which emptied into a vegetated swale.</p>
     </article>
     <?php include('includes/footer.php'); ?>
