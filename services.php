bd954a74cc63fd86da6b21b76978dbb342d3d922
Taylor Johnson <webninjataylor@gmail.com>
2013-05-08 15:31:24 -0400

    Services descriptions

--- services.php
+++ services.php
@@ -11,12 +11,20 @@
     <article>
         <h1>Services</h1>
         <ul>
-            <li>Stormwater Engineering and Design</li>
-            <li>Watershed/Municipality Modeling, Monitoring and Assessment</li>
-            <li>Local Program and Model Ordinance Development Assistance</li>
-            <li>Local, State and Federal Stormwater Regulation Interpretation and Guidance</li>
-            <li>Stormwater Management Feasibility Studies for Watersheds and/or Municipalities</li>
+            <li><strong>Stormwater Engineering and Design</strong><br>
+                Given the firm’s extensive design experience, no stormwater best management practice (BMP) is too complex for Stormwater Solutions and Services (SSS)! We can design simple, traditional practices or create innovative designs to meet unique criteria. In either case, the end result promises to be structurally sound, effective at controlling stormwater runoff, compliant with all applicable regulations and aesthetically pleasing. Our firm will produce high-quality, construction-ready documents complete with a Professional Engineer’s stamp. SSS also offers construction oversight of designed projects to ensure they are built as anticipated and function as intended.</li>
+            <li><strong>Stormwater Practice Inspection, Maintenance, and Restoration</strong><br>
+                Existing stormwater practices need to be inspected and maintained on a regular basis to ensure they are functioning properly. Many states and/or municipalities are requiring annual inspections of practices by certified individuals. Kathy DeBusk is certified as a North Carolina BMP Inspector, as well as a Professional Engineer in the states of Virginia and North Carolina, and can perform these annual inspections for property owners. SSS will complete and submit the required paperwork and advise owners about necessary maintenance and/or repairs. SSS is also able to fulfill routine maintenance requirements for existing practices to help them pass annual inspections without needing extensive, costly repairs. For practices that are no longer functioning as designed and intended, SSS provides rehabilitative analyses and restoration plans. In many cases, SSS can perform the necessary actions to repair the failed BMP and restore it to full functionality.</li>
+            <li><strong>Watershed/Municipality Modeling, Monitoring and Assessment</strong><br>
+                Stormwater regulations at the federal, state and local levels often require modeling, monitoring or assessment efforts to determine if/how municipalities can meet runoff and nutrient loading goals, such as those defined by Total Maximum Daily Loads (TMDLs). SSS can develop custom modeling tools for municipalities to aid in the implementation of a stormwater program, apply existing models and tools to estimate nutrient loadings and reductions provided by management practices and strategies, as well as develop monitoring plans and conduct water quality or hydrologic monitoring to assess watershed health and compliance.</li>
+            <li><strong>Local Program and Ordinance Development Assistance</strong><br>
+                The implementation of new stormwater regulations may require the development of local programs or ordinances to ensure compliance. Ms. DeBusk has extensive experience with the implementation of the Jordan and Falls Lake Nutrient Strategies in North Carolina and was involved in the development of model local programs and ordinances. SSS can assist clients with the development and implementation of programs and/or ordinances in accordance with any pertinent regulations.</li>
+            <li><strong>Local, State and Federal Stormwater Regulation Interpretation and Guidance</strong><br>
+                SSS can assist clients in interpreting and understanding regulations and how they pertain to the client’s situation. SSS can also serve as a liaison between clients and government/regulatory agencies to ensure regulations are met without exerting unnecessary effort and cost.</li>
+            <li><strong>Stormwater Management Feasibility Studies for Watersheds and/or Municipalities</strong><br>
+                SSS offers the valuable service of performing stormwater feasibility studies for municipalities and/or watersheds. Having conducted these analyses on over 8 watersheds in North Carolina, SSS can produce a valuable, high-quality product that allows clients to determine which management practices will result in the largest nutrient/sediment reductions for the least cost. Clients will be provided with detailed information for each potential management strategy and practice, including location, design specifications, estimated costs and expected pollutant load reductions.</li>
         </ul>
+        <p>These are just a few of the services we provide. If you are in need of assistance that is not discussed on this page, please do not hesitate to give us a call!</p>
     </article>
     <?php include('includes/footer.php'); ?>
 </body>
